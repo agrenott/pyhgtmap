0dde9176a57ec7decffb840d0634c7cf916be585
Adrian Dempwolff <phyghtmap@aldw.de>
2010-01-31 14:52:46 +0100

    Import version 1.11 from sources

--- phyghtmap/NASASRTMUtil.py
+++ phyghtmap/NASASRTMUtil.py
@@ -3,33 +3,14 @@ import os
 from BeautifulSoup import BeautifulSoup
 import zipfile
 
-############################################################
-### general vriables #######################################
-############################################################
-
-hgtSaveDir = "hgt"
-
-############################################################
-### NASA SRTM specific variables ###########################
-############################################################
-
-NASAhgtFileServerRe = "http://dds.cr.usgs.gov/srtm/version2_1/SRTM%s"
-NASAhgtFileDirs = {3: ["Africa", "Australia", "Eurasia", "Islands", "North_America",
+hgtFileServerRe = "http://dds.cr.usgs.gov/srtm/version2_1/SRTM%s"
+hgtFileDirs = {3: ["Africa", "Australia", "Eurasia", "Islands", "North_America",
                "South_America"],
                1: ["Region_0%i"%i for i in range(1, 8)]}
 
-NASAhgtSaveSubDirRe = "SRTM%i"
-NASAhgtIndexFileRe = os.path.join(hgtSaveDir, "hgtIndex_%i.txt")
-
-############################################################
-### www.vierfinderpanoramas.org specific variables #########
-############################################################
-
-VIEWfileDictPageRe = "http://www.viewfinderpanoramas.org/Coverage%%20map%%20viewfinderpanoramas_org%i.htm"
-
-VIEWhgtSaveSubDirRe = "VIEW%i"
-VIEWhgtIndexFileRe = os.path.join(hgtSaveDir, "viewfinderHgtIndex_%i.txt")
-
+hgtSaveDir = "hgt"
+hgtSaveSubDirRe = "SRTM%i"
+hgtIndexFileRe = os.path.join(hgtSaveDir, "hgtIndex_%i.txt")
 
 def calcBbox(area):
 	"""calculates the appropriate bouding box for the needed files
@@ -65,66 +46,41 @@ def calcBbox(area):
 			bboxMaxLat = int(maxLat) + 1
 	return bboxMinLon, bboxMinLat, bboxMaxLon, bboxMaxLat
 	
-def makeFileNamePrefixes(bbox, lowercase=False):
-	"""generates a list of filename prefixes of the files containing data within the
+def makeFileNames(bbox):
+	"""generates a list of filenames of the files containing data within the
 	bounding box.
 	"""
 	minLon, minLat, maxLon, maxLat = bbox
 	lon = minLon
-	prefixes = []
-	while lon <= maxLon:
+	filenames = []
+	while lon < maxLon:
 		if lon < 0:
 			lonSwitch = "W"
 		else:
 			lonSwitch = "E"
 		lat = minLat
-		while lat <= maxLat:
+		while lat < maxLat:
 			if lat < 0:
 				latSwitch = "S"
 			else:
 				latSwitch = "N"
-			prefixes.append("%s%s%s%s"%(latSwitch, str(abs(lat)).rjust(2, '0'),
+			filenames.append("%s%s%s%s.hgt.zip"%(latSwitch, str(abs(lat)).rjust(2, '0'),
 				lonSwitch, str(abs(lon)).rjust(3, '0')))
 			lat += 1
-			if minLat == maxLat or lat == maxLat:
-				break
 		lon += 1
-		if minLon == maxLon or lon == maxLon:
-			break
-	if lowercase:
-		return [p.lower() for p in prefixes]
-	else:
-		return prefixes
+	return filenames
 
-def makeFileNames(bbox, resolution, viewfinder):
-	"""generates a list of filenames of the files containing data within the
-	bounding box.  If <viewfinder> exists, this data is preferred to NASA SRTM
-	data.
-	"""
-	areas = makeFileNamePrefixes(bbox)
-	areaDict = {}
-	for a in areas:
-		NASAurl = getNASAUrl(a, resolution)
-		areaDict[a] = NASAurl
-	if viewfinder:
-		for a in areas:
-			VIEWurl = getViewUrl(a, viewfinder)
-			if not VIEWurl:
-				continue
-			areaDict[a] = VIEWurl
-	return areaDict
-
-def makeNasaHgtIndex(resolution):
+def makeHgtIndex(resolution):
 	"""generates an index file for the NASA SRTM server.
 	"""
-	hgtIndexFile = NASAhgtIndexFileRe%resolution
-	hgtFileServer = NASAhgtFileServerRe%resolution
+	hgtIndexFile = hgtIndexFileRe%resolution
+	hgtFileServer = hgtFileServerRe%resolution
 	print "generating index in %s ..."%hgtIndexFile
 	try:
 		index = open(hgtIndexFile, 'w')
 	except:
 		raise IOError("could not open %s for writing"%hgtIndexFile)
-	for continent in NASAhgtFileDirs[resolution]:
+	for continent in hgtFileDirs[resolution]:
 		index.write("[%s]\n"%continent)
 		url = "/".join([hgtFileServer, continent])
 		continentHtml = urllib.urlopen(url)
@@ -136,41 +92,15 @@ def makeNasaHgtIndex(resolution):
 				index.write("%s\n"%zipFilename)
 	print "DONE"
 
-def makeViewHgtIndex(resolution):
-	"""generates an index file for the viewfinder hgt files.
+def getUrl(file, resolution):
+	"""determines the download url for a given filename.
 	"""
-	hgtIndexFile = VIEWhgtIndexFileRe%resolution
-	hgtFileServer = NASAhgtFileServerRe%resolution
-	print "generating index in %s ..."%hgtIndexFile
-	try:
-		index = open(hgtIndexFile, 'w')
-	except:
-		raise IOError("could not open %s for writing"%hgtIndexFile)
-	hgtDictUrl = VIEWfileDictPageRe%resolution
-	areaDict = dict([(a["title"], a["href"]) for a in
-		BeautifulSoup(urllib.urlopen(hgtDictUrl).read()).findAll("area")])
-	zipFileDict = {}
-	for areaName, zipFileUrl in areaDict.items():
-		if not zipFileDict.has_key(zipFileUrl):
-			zipFileDict[zipFileUrl] = []
-		zipFileDict[zipFileUrl].append(areaName.upper())
-	for zipFileUrl in sorted(zipFileDict):
-		index.write("[%s]\n"%zipFileUrl)
-		for areaName in zipFileDict[zipFileUrl]:
-			index.write(areaName + "\n")
-	index.close()
-	print "DONE"
-
-def getNASAUrl(area, resolution):
-	"""determines the NASA download url for a given area.
-	"""
-	file = "%s.hgt.zip"%area
-	hgtIndexFile = NASAhgtIndexFileRe%resolution
-	hgtFileServer = NASAhgtFileServerRe%resolution
+	hgtIndexFile = hgtIndexFileRe%resolution
+	hgtFileServer = hgtFileServerRe%resolution
 	try:
 		os.stat(hgtIndexFile)
 	except:
-		makeNasaHgtIndex(resolution)
+		makeHgtIndex(resolution)
 	index = open(hgtIndexFile, 'r').readlines()
 	fileMap = {}
 	for line in index:
@@ -184,73 +114,35 @@ def getNASAUrl(area, resolution):
 	url = '/'.join([hgtFileServer, fileMap[file], file])
 	return url
 
-def getViewUrl(area, resolution):
-	"""determines the viewfinder download url for a given area.
-	"""
-	hgtIndexFile = VIEWhgtIndexFileRe%resolution
-	try:
-		os.stat(hgtIndexFile)
-	except:
-		makeViewHgtIndex(resolution)
-	index = open(hgtIndexFile, 'r').readlines()
-	fileMap = {}
-	for line in index:
-		line = line.strip()
-		if line.startswith("["):
-			url = line[1:-1]
-		else:
-			fileMap[line] = url
-	if not fileMap.has_key(area):
-		return None
-	url = fileMap[area]
-	return url
-
 def unzipFile(saveZipFilename):
-	"""unzip a zip file.
+	"""unzip a .hgt.zip file.
 	"""
+	saveFilename = saveZipFilename[0:-4]
 	print "unzipping file %s ..."%saveZipFilename
 	zipFile = zipfile.ZipFile(saveZipFilename)
 	for name in zipFile.namelist():
-		areaName = os.path.splitext(os.path.split(name)[-1])[0].upper().strip()
-		if not areaName:
-			continue
-		saveFilename = os.path.join(os.path.split(saveZipFilename)[0],
-			areaName + ".hgt")
 		saveFile = open(saveFilename, 'wb')
 		saveFile.write(zipFile.read(name))
 		saveFile.close()
-	os.remove(saveZipFilename)
+		os.remove(saveZipFilename)
 	print "DONE"
 
-def getFiles(area, resolution, viewfinder=False):
-	NASAhgtSaveSubDir = os.path.join(hgtSaveDir, NASAhgtSaveSubDirRe%resolution)
-	VIEWhgtSaveSubDir = os.path.join(hgtSaveDir, VIEWhgtSaveSubDirRe%viewfinder)
+def getFiles(area, resolution):
+	hgtSaveSubDir = os.path.join(hgtSaveDir, hgtSaveSubDirRe%resolution)
 	try:
 		os.stat(hgtSaveDir)
 	except:
 		os.mkdir(hgtSaveDir)
 	try:
-		os.stat(NASAhgtSaveSubDir)
+		os.stat(hgtSaveSubDir)
 	except:
-		os.mkdir(NASAhgtSaveSubDir)
-	try:
-		os.stat(VIEWhgtSaveSubDir)
-	except:
-		os.mkdir(VIEWhgtSaveSubDir)
+		os.mkdir(hgtSaveSubDir)
 	bbox = calcBbox(area)
-	print bbox
-	filesToDownload = makeFileNames(bbox, resolution, viewfinder)	
+	filesToDownload = makeFileNames(bbox)	
 	files = []
-	for area, url in sorted(filesToDownload.items()):
-		if not url:
-			print "no file for area %s found on server."%area
-			continue
-		if "viewfinderpanoramas" in url:
-			hgtSaveSubDir = VIEWhgtSaveSubDir
-		else:
-			hgtSaveSubDir = NASAhgtSaveSubDir
-		saveZipFilename = os.path.join(hgtSaveSubDir, url.split("/")[-1])
-		saveFilename = os.path.join(hgtSaveSubDir, "%s.hgt"%area)
+	for file in filesToDownload:
+		saveZipFilename = os.path.join(hgtSaveSubDir, file)
+		saveFilename = saveZipFilename[0:-4]
 		try:
 			os.stat(saveFilename)
 			wantedSize = 2 * (3600/resolution + 1)**2
@@ -263,12 +155,16 @@ def getFiles(area, resolution, viewfinder=False):
 				os.stat(saveZipFilename)
 				unzipFile(saveZipFilename)
 			except:
-				print "downloading file %s to %s ..."%(url, saveZipFilename)
-				urllib.urlretrieve(url, filename=saveZipFilename)
-				try:
-					unzipFile(saveZipFilename)
-				except:
-					print "file %s from %s is not a zip file"%(saveZipFilename, url)
+				url = getUrl(file, resolution)
+				if url:
+					print "donwloading file %s to %s ..."%(url, saveZipFilename)
+					urllib.urlretrieve(url, filename=saveZipFilename)
+					try:
+						unzipFile(saveZipFilename)
+					except:
+						print "file %s from %s is not a zip file"%(saveZipFilename, url)
+				else:
+					print "file %s not found on server."%file
 		try:
 			os.stat(saveFilename)
 			files.append(saveFilename)
