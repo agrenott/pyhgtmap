0fcf818a6a45eed40df0d54c8921e27be3d12757
Aurélien Grenotton <agrenott@gmail.com>
2023-03-03 23:13:46 +0100

    Add dependencies and visual unit test
    Allows installing the package and dependencies using pip.
    Visual unit test to prepare for future libraries upgrades which might
    slightly change behavior.

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
 from setuptools import setup, find_packages
-#from setuptools import setup, find_packages, Extension
+
+# from setuptools import setup, find_packages, Extension
 import warnings
+
 warnings.filterwarnings("ignore", "Unknown distribution option")
 
-#from phyghtmap import __version__
+# from phyghtmap import __version__
 
-setup(name="phyghtmap",
-	version="2.23",
-	packages = find_packages(),
-	description="OSM contour lines creator.",
-	include_data_files=True,
-	author="Adrian Dempwolff",
-	author_email="phyghtmap@aldw.de",
-	url="http://katze.tfiu.de/projects/phyghtmap/",
-	long_description="""phyghtmap creates openstreetmap suitable contour lines from NASA SRTM data.""",
-	license="GPLv2+",
-	#ext_modules=[
-		#Extension("phyghtmap.pbfint", ["phyghtmap/pbfintmodule.c"])
-	#],
-	entry_points = {
-		'console_scripts': [
-			'phyghtmap = phyghtmap.main:main',
-		],
-	},
+setup(
+    name="phyghtmap",
+    version="2.23",
+    packages=find_packages(),
+    description="OSM contour lines creator.",
+    include_data_files=True,
+    author="Adrian Dempwolff",
+    author_email="phyghtmap@aldw.de",
+    url="http://katze.tfiu.de/projects/phyghtmap/",
+    long_description="""phyghtmap creates openstreetmap suitable contour lines from NASA SRTM data.""",
+    license="GPLv2+",
+    # ext_modules=[
+    # Extension("phyghtmap.pbfint", ["phyghtmap/pbfintmodule.c"])
+    # ],
+    entry_points={
+        "console_scripts": [
+            "phyghtmap = phyghtmap.main:main",
+        ],
+    },
+    install_requires=[
+		# Do NOT pin GDAL version to ease installing it via OS package manager (due to many dependencies)
+        "GDAL",
+        "matplotlib==3.4.3",
+        "contourpy~=1.0.7",
+        "bs4==0.0.1",
+        "numpy==1.24.2",
+    ],
 )
-
--- tests/test_hgt.py
+++ tests/test_hgt.py
@@ -1,5 +1,7 @@
 import os
+import matplotlib.pyplot as plt
 import numpy
+import pytest
 from types import SimpleNamespace
 from typing import List
 
@@ -8,16 +10,21 @@ from phyghtmap import hgt
 TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.relpath(__file__)), "data")
 
 
+@pytest.fixture
+def toulon_tiles() -> List[hgt.hgtTile]:
+    hgt_file = hgt.hgtFile(os.path.join(TEST_DATA_PATH, "N43E006.hgt"), 0, 0)
+    # Fake command line parser output
+    options = SimpleNamespace(area=None, maxNodesPerTile=0, contourStepSize=20)
+    tiles: List[hgt.hgtTile] = hgt_file.makeTiles(options)
+    return tiles
+
+
 class TestTile:
     @staticmethod
-    def test_contourLines():
+    def test_contourLines(toulon_tiles: List[hgt.hgtTile]) -> None:
         """Test contour lines extraction from hgt file."""
-        hgt_file = hgt.hgtFile(os.path.join(TEST_DATA_PATH, "N43E006.hgt"), 0, 0)
-        # Fake command line parser output
-        options = SimpleNamespace(area=None, maxNodesPerTile=0, contourStepSize=20)
-        tiles: List[hgt.hgtTile] = hgt_file.makeTiles(options)
-        assert tiles
-        elevations, contour_data = tiles[0].contourLines()
+        assert toulon_tiles
+        elevations, contour_data = toulon_tiles[0].contourLines()
         assert elevations == range(0, 1940, 20)
         assert contour_data
         # Get the countours for 20m elevation
@@ -43,3 +50,19 @@ class TestTile:
                 ]
             ),
         )
+
+    @staticmethod
+    @pytest.mark.mpl_image_compare(baseline_dir="data", filename="toulon_ref.png")
+    def test_draw_contours_Toulon(toulon_tiles: List[hgt.hgtTile]) -> plt.Figure:
+        """Rather an end-to-end test.
+        Print contours in Toulons area to assert overall result, even if contours are not exactly the same (eg. algo evolution).
+        To compare output, run `pytest --mpl`
+        """
+        elevations, contour_data = toulon_tiles[0].contourLines()
+        fig = plt.figure()
+        for elev in range(0, 500, 100):
+            for contour in contour_data.trace(elev)[0]:
+                x, y = zip(*contour)
+                plt.plot(x, y, color="black")
+        # plt.savefig(os.path.join(TEST_DATA_PATH, "toulon_out.png"))
+        return fig
