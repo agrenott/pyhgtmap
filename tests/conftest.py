621ad7767a758a578313e17b03bb555ef56d9040
Aurélien Grenotton <agrenott@gmail.com>
2025-11-23 19:33:55 +0100

    Fix negative elevations
    Use signed int for elevation.
    Close #68

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+from collections.abc import Generator
+from typing import Any
+from unittest.mock import patch
+
+import configargparse
 import pytest
 
 from pyhgtmap.configuration import Configuration
@@ -6,3 +11,13 @@ from pyhgtmap.configuration import Configuration
 @pytest.fixture
 def configuration() -> Configuration:
     return Configuration()
+
+
+@pytest.fixture(autouse=True)
+def _disable_config_file() -> Generator[None, Any, None]:
+    """Disable the configuration file for the tests."""
+    with patch.object(
+        configargparse.ArgumentParser, "_open_config_files"
+    ) as _open_config_files_mock:
+        _open_config_files_mock.return_value = []
+        yield
